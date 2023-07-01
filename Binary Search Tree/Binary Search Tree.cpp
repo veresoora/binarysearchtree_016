cb545104dee191809264b497a48167d66f40e860
veresoora <lahanabira@gmail.com>
2023-07-01 21:42:55 +0700

    Menangani case 1 dalam pernyataan switch untuk insert operation

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -136,7 +136,14 @@ int main()
 		cout << endl;
 
 		switch (ch) {
-
+		case '1':
+		{
+			cout << "Enter a word : ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
 		}
 	}
 }
