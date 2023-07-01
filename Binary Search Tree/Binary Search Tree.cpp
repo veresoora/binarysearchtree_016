9a2b2f278ea68777c0a02adc009c22d635fba27e
veresoora <lahanabira@gmail.com>
2023-07-01 21:28:41 +0700

    Menerapkan traversal postorder dalam fungsi

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -109,5 +109,11 @@ public:
 			cout << "Tree is empty" << endl;
 			return;
 		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
 	}
 };
